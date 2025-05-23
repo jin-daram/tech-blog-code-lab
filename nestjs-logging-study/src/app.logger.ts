36a3d56271f0e5a353a9fe663733a3da4efa32c8
jin-daram <jindaram1999@gmail.com>
2025-05-23 17:46:42 +0900

    store🧑‍💻: nestjs-logging-study 데이터 백업

--- nestjs-logging-study/src/app.logger.ts
+++ nestjs-logging-study/src/app.logger.ts
@@ -1,5 +1,7 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format } from "winston";
+import LokiTransport from "winston-loki";
 
+/*
 const logger = createLogger({
     level: 'info',
     format: format.combine( 
@@ -14,5 +16,22 @@ const logger = createLogger({
         new transports.File({ filename: 'logs/error.log', level: 'error'}),
     ]
 })
+*/
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    transports: [
+        new LokiTransport({
+            host: 'http://localhost:3100',
+            labels: { app: 'test-app', env: 'local' },
+            json: true,
+            replaceTimestamp: true,
+        }),
+    ],
+});
 
 export default logger;
